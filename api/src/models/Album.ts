20cc3ea57f5430a5ac43c59168a01076466d6f21
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-04-16 10:26:10 +0600

    Added models Composition.ts and Album.ts

---
+++ api/src/models/Album.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const AlbumSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  singer: {
+    type: Schema.Types.ObjectId,
+    ref: 'Singer',
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
+  image: String,
+});
+
+const Album  = mongoose.model('Album', AlbumSchema);
+
+export default Album;
