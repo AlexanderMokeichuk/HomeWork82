82875f196ab24b1fd5c9f19d3d61da6c369676c7
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-04-18 18:00:47 +0600

    #HW-83 Added new interfaces

--- api/src/type.d.ts
+++ api/src/type.d.ts
@@ -1,18 +1,28 @@
-export interface SingerFront {
+import Types = module;
+
+export interface ArtistFront {
   name: string,
   image: string | null,
-  description?: string,
+  description: string | null,
 }
 
 export interface AlbumFront {
   name: string,
-  singer: string,
+  artist: Types.ObjectId,
   createdAt: string,
   image: string | null,
 }
 
 export interface TrackFront {
   name: string,
-  album: string,
+  album: Types.ObjectId,
   duration: string,
+}
+
+export interface ArtistApi extends ArtistFront {
+  _id: string
+}
+export interface AlbumApi extends AlbumFront {
+  _id: string,
+  artist: ArtistApi,
 }
